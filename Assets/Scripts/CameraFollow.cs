5ef78297fe736adb8ab7dd5fcb746269c5682d31
chardzard <richard.sage28@gmail.com>
2016-05-25 16:53:00 -0300

    Cleaned up unused standard assets. Added player character movement and camera follow.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = target.position + offset;
+
+        //transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.deltaTime);
+	}
+}
