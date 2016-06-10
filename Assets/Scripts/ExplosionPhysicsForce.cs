89ebc5a61fbddcf5722125db681e73926afbf242
chardzard <richard.sage28@gmail.com>
2016-06-10 16:04:26 -0300

    Finished exploding bullets. Bullets explode on impact or on death. Enemies explode on dying. Scoring working.

--- Assets/Scripts/ExplosionPhysicsForce.cs
+++ Assets/Scripts/ExplosionPhysicsForce.cs
@@ -4,39 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
-namespace UnityStandardAssets.Effects
-{
-    public class ExplosionPhysicsForce : MonoBehaviour
-    {
+namespace UnityStandardAssets.Effects {
+    public class ExplosionPhysicsForce : MonoBehaviour {
         public float explosionForce = 4;
 
 
-        private IEnumerator Start()
-        {
+        private IEnumerator Start() {
             // wait one frame because some explosions instantiate debris which should then
             // be pushed by physics force
             yield return null;
 
             float multiplier = GetComponent<ParticleSystemMultiplier>().multiplier;
 
-            float r = 10*multiplier;
+            float r = 10 * multiplier;
             var cols = Physics.OverlapSphere(transform.position, r);
             var rigidbodies = new List<Rigidbody>();
-            foreach (var col in cols)
-            {
-                if (col.attachedRigidbody != null && !rigidbodies.Contains(col.attachedRigidbody))
-                {
+            foreach (var col in cols) {
+                if (col.attachedRigidbody != null && !rigidbodies.Contains(col.attachedRigidbody)) {
                     rigidbodies.Add(col.attachedRigidbody);
                 }
                 AICharacterControl AI = col.GetComponent<AICharacterControl>();
                 if (AI != null) {
                     AI.Damage(1);
-                }                
+                }
             }
-            foreach (var rb in rigidbodies)
-            {
-                rb.AddExplosionForce(explosionForce*multiplier, transform.position, r, 0, ForceMode.Impulse);
+            foreach (var rb in rigidbodies) {
+                rb.AddExplosionForce(explosionForce * multiplier, transform.position, r, 0, ForceMode.Impulse);
             }
+            StartCoroutine(destroy());
         }
-    }
+
+        IEnumerator destroy() {
+            yield return new WaitForSeconds(5f);
+            Destroy(gameObject);
+        }
+    }    
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
@@ -9,11 +10,15 @@ public class GameController : MonoBehaviour {
     public Transform playerPosition;
     public Transform[] spawnPoints;
     public AICharacterControl basicEnemy;
+    public Text score;
+
+    static int scoreNumber = 0;
 
     // Use this for initialization
     void Start () {
         InvokeRepeating("SpawnBasic", spawnInterval * difficulty, spawnInterval * difficulty);
-	}
+        score.text = scoreNumber.ToString();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,5 +29,11 @@ public class GameController : MonoBehaviour {
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
         AICharacterControl spawn = (AICharacterControl) Instantiate(basicEnemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
         spawn.target = playerPosition;
+        spawn.controller = this;
+    }
+
+    public void IncreaseScore(int increase) {
+        scoreNumber += increase;
+        score.text = scoreNumber.ToString();
     }
 }
